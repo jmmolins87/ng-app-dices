fc23f14f6b964ff629d26695226ffdf5504750a7
Juanma <jmmolins87@gmail.com>
2023-02-20 11:22:09 +0100

    fixed app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'emptyangularproject';
+
+  public diceLeft: string = './../assets/images/dice1.png';
+  public diceRight: string = './../assets/images/dice4.png';
+  public numOne!: number;
+  public numTwo!: number;
+
+  throwDices() {
+    this.numOne = Math.round( Math.random() * 5 ) + 1;
+    this.numTwo = Math.round( Math.random() * 5 ) + 1;
+    this.diceLeft = `../assets/images/dice${ this.numOne }.png`;
+    this.diceRight = `../assets/images/dice${ this.numTwo }.png`;
+  }
 }
